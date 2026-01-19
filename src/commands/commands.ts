a8a9ddd72e1cec1209dfc7eff73f256687bcb898
James Gauld <james.gauld@mckennaconsultants.com>
2026-01-19 15:39:08 +0000

    Initial commit

---
+++ src/commands/commands.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/**
+ * Shows a notification when the add-in command is executed.
+ * @param event
+ */
+// function runDemo(event: Office.AddinCommands.Event | null) {
+//   console.log("[ddguo][commands.ts]: command received");
+//   if (event) {
+//     event.completed();
+//   }
+// }
+
+// // /* global Office */
+
+// // Office.onReady(() => {
+// //   // If needed, Office.js is ready to be called.
+// //   console.log("[ddguo][commands.ts]: office ready");
+// //   runDemo(null);
+// // });
+
+// // Register the function with Office. 
+// Office.actions.associate("runDemo", runDemo);
+
---
+++ src/taskpane/taskpane.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  console.log("[ddguo][taskpane.ts]: office ready");
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  console.log("[ddguo][taskpane.ts]: run is executed.");
+
+  try {
+    await Excel.run({ mergeUndoGroup: true }, async (context) => {
+      const currentWorkbook = context.workbook;
+      const activeCell = currentWorkbook.getActiveCell();
+
+      // Set the standardWidth property of the first worksheet to clear the Undo stack.
+      currentWorkbook.worksheets.getActiveWorksheet().standardWidth = 8.43;
+
+      // Insert dummy data to current cell
+      activeCell.values = [["TEST"]];
+
+      await context.sync();
+      console.log("[ddguo][taskpane.ts]: Active cell updated with Custom XML Part ID.");
+    });
+  } catch (error) {
+    console.error(error);
+    console.error("Error message:", error.message);
+    console.error("Error stack:", error.stack);
+    console.error("Error code:", error.code);
+    console.error("Error traceMessages:", error.traceMessages);
+    console.error("Error debugInfo:", error.debugInfo);
+  }
+}
+
+function runDemo(event: Office.AddinCommands.Event | null) {
+  console.log("[ddguo][taskpane.ts]: command received");
+  if (event) {
+    event.completed();
+  }
+}
+
+// Register the function with Office.
+Office.actions.associate("runDemo", runDemo);
